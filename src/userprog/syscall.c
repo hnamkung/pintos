c2bf0ea4fd86e088fa48bc59fe42d2f605ff4d47
Henry Namkoong <hnamkoong1@gmail.com>
2015-04-03 08:53:19 +0400

    ready to develop syscall 22.3%

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -6,8 +6,20 @@
 #include "threads/thread.h"
 
 static void syscall_handler (struct intr_frame *);
-static void syscall_write(struct intr_frame *f);
+
+static void syscall_halt(struct intr_frame *f);
 static void syscall_exit(struct intr_frame *f);
+static void syscall_exec(struct intr_frame *f);
+static void syscall_wait(struct intr_frame *f);
+static void syscall_create(struct intr_frame *f);
+static void syscall_remove(struct intr_frame *f);
+static void syscall_open(struct intr_frame *f);
+static void syscall_filesize(struct intr_frame *f);
+static void syscall_read(struct intr_frame *f);
+static void syscall_write(struct intr_frame *f);
+static void syscall_seek(struct intr_frame *f);
+static void syscall_tell(struct intr_frame *f);
+static void syscall_close(struct intr_frame *f);
 
 void
 syscall_init (void) 
@@ -22,13 +34,90 @@ syscall_handler (struct intr_frame *f UNUSED)
     if(SYS_NUM == SYS_WRITE) {
          syscall_write(f);
     }
+    else if(SYS_NUM == SYS_HALT) {
+        syscall_halt(f);
+    }
     else if(SYS_NUM == SYS_EXIT) {
         syscall_exit(f);
+    }
+    else if(SYS_NUM == SYS_EXEC) {
+        syscall_exec(f);
+    }
+    else if(SYS_NUM == SYS_WAIT) {
+        syscall_wait(f);
+    }
+    else if(SYS_NUM == SYS_CREATE) {
+        syscall_create(f);
+    }
+    else if(SYS_NUM == SYS_REMOVE) {
+        syscall_remove(f);
+    }
+    else if(SYS_NUM == SYS_OPEN) {
+        syscall_open(f);
+    }
+    else if(SYS_NUM == SYS_FILESIZE) {
+        syscall_halt(f);
+    }
+    else if(SYS_NUM == SYS_READ) {
+        syscall_read(f);
+    }
+    else if(SYS_NUM == SYS_WRITE) {
+        syscall_write(f);
+    }
+    else if(SYS_NUM == SYS_SEEK) {
+        syscall_seek(f);
+    }
+    else if(SYS_NUM == SYS_TELL) {
+        syscall_tell(f);
+    }
+    else if(SYS_NUM == SYS_CLOSE) {
+        syscall_close(f);
     }
     else {
         thread_exit ();
     }
 }
+
+static void syscall_exit(struct intr_frame *f)
+{
+    int status = *(int *)(f->esp+4);
+    struct thread *t = thread_current();
+    printf("%s: exit(%d)\n", t->name, status);
+    thread_exit();
+}
+
+static void syscall_halt(struct intr_frame *f)
+{
+}
+
+static void syscall_exec(struct intr_frame *f)
+{
+}
+
+static void syscall_wait(struct intr_frame *f)
+{
+}
+
+static void syscall_create(struct intr_frame *f)
+{
+}
+
+static void syscall_remove(struct intr_frame *f)
+{
+}
+
+static void syscall_open(struct intr_frame *f)
+{
+}
+
+static void syscall_filesize(struct intr_frame *f)
+{
+}
+
+static void syscall_read(struct intr_frame *f)
+{
+}
+
 static void syscall_write(struct intr_frame *f)
 {
     int fd = *(int *)(f->esp + 4);
@@ -42,10 +131,16 @@ static void syscall_write(struct intr_frame *f)
     }
 }
 
-static void syscall_exit(struct intr_frame *f)
+
+static void syscall_seek(struct intr_frame *f)
 {
-    int status = *(int *)(f->esp+4);
-    struct thread *t = thread_current();
-    printf("%s: exit(%d)\n", t->name, status);
-    thread_exit();
 }
+
+static void syscall_tell(struct intr_frame *f)
+{
+}
+
+static void syscall_close(struct intr_frame *f)
+{
+}
+
